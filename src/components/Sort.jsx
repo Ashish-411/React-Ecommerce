4cb3a188f0e17698dbde6f7dbb8abf708c895ac0
Ashish-411 <ashishmaleku123@gmail.com>
2025-12-23 08:38:26 +0545

    sorting product

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { BsFillGridFill, BsList } from "react-icons/bs";
 import { useFilterContext } from "../context/filter_context";
 function Sort(){
-    const {filter_products,grid_view,setGridView,setListView} = useFilterContext();
+    const {filter_products,grid_view,setGridView,setListView,sorting} = useFilterContext();
     return(
         <Wrapper className="sort-section">
             <div className="sorting-list--grid">
@@ -18,7 +18,21 @@ function Sort(){
             <div className="product-data">
                 <p>{filter_products.length} Product Available</p>
             </div>
-            <div className="sort-selection"></div>
+            <div className="sort-selection">
+              <form action="#">
+                <label htmlFor="sort"></label>
+                <select name="sort" id="sort" className="sort-selection--style" onClick={sorting}>
+                  <option value="lowest">Price(lowest)</option>
+                  <option value="#" disabled></option>
+                  <option value="highest">Price(Highest)</option>
+                  <option value="#" disabled></option>
+                  <option value="a-z">Product(a-z)</option>
+                  <option value="#" disabled></option>
+                  <option value="z-a">Product(z-a)</option>
+                </select>
+              </form>
+              
+            </div>
             
         </Wrapper>
     );
--- src/reducer/filterReducer.js
+++ src/reducer/filterReducer.js
@@ -16,7 +16,40 @@ const filterReducer = (state,action) =>{
                 ...state,
                 grid_view: false,
             }
-
+        case "GET_SORT_VALUE":
+            let userSortValue = document.getElementById("sort");
+            let sort_value = userSortValue.options[userSortValue.selectedIndex].value;
+            return{
+                ...state,
+                sorting_value: sort_value,
+            }
+        case "SORTING_PRODUCTS":
+            let newSortData;
+            let tempSortData = [...action.payload];
+            if(state.sorting_value === "lowest"){
+                newSortData = tempSortData.sort((a,b) => {
+                    return a.price - b.price;
+                });
+            }
+            if(state.sorting_value === "highest"){
+                newSortData = tempSortData.sort((a,b) => {
+                    return b.price - a.price;
+                });
+            }
+            if(state.sorting_value === "a-z"){
+                newSortData = tempSortData.sort((a,b) => {
+                    return a.title.localeCompare(b.title);
+                });
+            }
+            if(state.sorting_value === "z-a"){
+                newSortData = tempSortData.sort((a,b) => {
+                    return b.title.localeCompare(a.title);
+                });
+            }
+            return{
+                ...state,
+                filter_products:newSortData,
+            }
         default:
             return state;
     }
